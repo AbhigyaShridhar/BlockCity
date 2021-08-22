bb9d6555afa25e900e91b40f528684b02f93bde2
AbhigyaShridhar <55044948+AbhigyaShridhar@users.noreply.github.com>
2021-08-22 16:17:30 +0530

    Update settings.py

--- slam_dunks/slam_dunks/settings.py
+++ slam_dunks/slam_dunks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(-5z(^sg+woenrx^e@*&bx!the+v4i$f)#4o4drtx4a!(*++qk'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
