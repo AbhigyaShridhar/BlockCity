198b9c0645152121c69235f70e506a585d2198a6
AbhigyaShridhar <55044948+AbhigyaShridhar@users.noreply.github.com>
2021-08-22 15:39:42 +0530

    Update admin.py

--- slam_dunks/backend/admin.py
+++ slam_dunks/backend/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Match, City, Team, Venue, School
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Team)
+admin.site.register(Match)
+admin.site.register(City)
+admin.site.register(School)
+admin.site.register(Venue)
